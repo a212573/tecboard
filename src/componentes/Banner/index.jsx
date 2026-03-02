e8a165cedc2ce6c0b158c9a7b48e72cbf3a7aca4
Amanda Rocha Faga <113113176+a212573@users.noreply.github.com>
2026-03-02 14:02:50 -0300

    Delete src directory

--- src/componentes/Banner/index.jsx
+++
@@ -1,9 +0,0 @@
-import './banner.estilos.css'
-
-export function Banner() {
-    return (
-        <section className="banner">
-            <img className="banner img" src="../../../public/banner.png" alt="banner" />
-        </section>
-    )
-}
--- src/componentes/CampoDeEntrada/index.jsx
+++
@@ -1,5 +0,0 @@
-import './campo-entrada.estilos.css'
-
-export function CampoDeEntrada (props) {
-    return <input className='campo-entrada-form' {...props} />
-  }
